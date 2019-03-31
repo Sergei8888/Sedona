ded03fe5aa5c80e0ec81fae4e61cbfd0294b89d1
Sergei888 <sergeiclashroale@gmail.com>
2019-03-31 18:43:10 +0200

    Update main.js

--- Electron test/main.js
+++ Electron test/main.js
@@ -2,24 +2,24 @@ const path = require("path");
 const url = require("url");
 const {app, BrowserWindow} = require("electron");
 
-let appState; //Program status
+let win; //Program status
 
 function createWindow(){
-	appState = new BrowserWindow({
+	win = new BrowserWindow({
 		width: 1000, 
 		height: 500, 
 		icon: __dirname + "/img/programm-icon.png"
 	});
 
-	window.loadURL(url.format({
+	win.loadURL(url.format({
 		pathname: path.join(__dirname, "index.html"), 
 		protocol: "file:",
 		slashes: true
 	}));
 
-	window.webContents.openDevTools();
+	win.webContents.openDevTools();
 
-	window.on("closed", () => {
+	win.on("closed", () => {
 		win = null;
 	});
 }
