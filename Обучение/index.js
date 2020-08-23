606fe9776b16f3105d8a679c5ae8b79d319d8666
Sergei8888 <45688549+Sergei8888@users.noreply.github.com>
2020-08-23 19:32:13 +0200

    Add files via upload

---
+++ Обучение/index.js
@@ -0,0 +1,4 @@
+'use strict';
+let formSumbitBtn = document.getElementById("form__sumbit-btn"),
+    formAlert = document.getElementById("form_alert");
+formSumbitBtn.onclick = () => formAlert.style.display = "block";
