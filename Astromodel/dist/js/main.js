aea93024d21c730b2b086581ec137071f3914deb
Sergei8888 <45688549+Sergei8888@users.noreply.github.com>
2021-05-13 09:10:10 +0200

    Update main.js

--- Astromodel/dist/js/main.js
+++ Astromodel/dist/js/main.js
@@ -141,7 +141,7 @@ let vm = new Vue({
         },
 
         updateAnim() {
-            postData('http://95.55.247.243:80/api/test/dDha03LqkyCYI6NyRZysPXukX', this.formattedObjectList)
+            postData('http://xenofium-astromodel.herokuapp/api/test/dDha03LqkyCYI6NyRZysPXukX', this.formattedObjectList)
                 .then((data) => {
                     console.log(data); // JSON data parsed by `response.json()` call
                 });
