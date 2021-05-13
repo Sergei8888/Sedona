a241cf6585919535dde21a84136d9c76e7769654
Sergei8888 <sergeiclashroale@gmail.com>
2021-05-13 22:27:03 +0200

    update https

--- Astromodel/dist/js/main.js
+++ Astromodel/dist/js/main.js
@@ -141,7 +141,7 @@ let vm = new Vue({
         },
 
         updateAnim() {
-            postData('http://xenofium-astromodel.herokuapp.com/api/test/dDha03LqkyCYI6NyRZysPXukX', this.formattedObjectList)
+            postData('https://xenofium-astromodel.herokuapp.com/api/test/dDha03LqkyCYI6NyRZysPXukX', this.formattedObjectList)
                 .then((data) => {
                     console.log(data); // JSON data parsed by `response.json()` call
                 });
