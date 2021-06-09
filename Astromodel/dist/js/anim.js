44525d6ec7053c8339b28406958867d8603273a7
leklid <leo-oganisian@yandex.ru>
2021-06-09 21:36:24 +0200

    gg

--- Astromodel/dist/js/anim.js
+++ Astromodel/dist/js/anim.js
@@ -45,6 +45,7 @@ function onData(data) {
 function draw() {
     push()
     let size = width
+    background(34)
     noStroke()
     fill(22)
     circle(size * 0.5, size * 0.5, size)
@@ -72,16 +73,37 @@ function draw() {
         // }
         // noStroke()
 
+        let coordXarray = []
+        let coordYarray = []
+
         for (id in BALLS.colors) {
             fill(BALLS.colors[id])
             let angle = BALLS.frames[BALLS.current][id]
             let x = size * 0.5 + Math.cos(angle) * size * 0.45
             let y = size * 0.5 - Math.sin(angle) * size * 0.45
+            coordXarray.push(x)
+            coordYarray.push(y)
             circle(x, y, size * 0.07)
         }
 
+        let coordX = coordXarray.reduce((a, b) => a + b) / coordXarray.length
+        let coordY = coordYarray.reduce((a, b) => a + b) / coordYarray.length
+        BALLS.massCenter.push([coordX, coordY])
+
+        noFill()
+        stroke(255, 70)
+        strokeWeight(size * 0.01)
+        beginShape()
+        for (let i = BALLS.current; i > Math.max(0, BALLS.current - 100); i--) {
+            vertex(...BALLS.massCenter[i])
+        }
+        endShape()
+        noStroke()
+        fill(255, 100)
+        circle(coordX, coordY, size * 0.07)
+
         BALLS.current++
-            if (BALLS.current === BALLS.framesCount) { BALLS.current = 0 }
+        if (BALLS.current === BALLS.framesCount) { BALLS.current = 0 }
     }
     pop()
 }
