55968ae037c679d8116f17305c5bb0176adbd358
Sergei Kuzmenkov <45688549+Sergei8888@users.noreply.github.com>
2022-04-17 11:30:02 +0200

    Delete Обучение directory

--- Обучение/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-let formSumbitBtn = document.getElementById("form__sumbit-btn"),
-    formAlert = document.getElementById("form_alert");
-formSumbitBtn.onclick = () => formAlert.style.display = "inline";
